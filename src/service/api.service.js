b0d8466826318e8cd6d670465f1101220e06490f
Menunoai <phik07tx02102002@gmail.com>
2024-11-15 20:22:47 +0700

    video 72

--- src/service/api.service.js
+++ src/service/api.service.js
@@ -9,14 +9,19 @@ const createUserAPI = (fullName, email, password, phone) => {
         password: password,
         phone: phone
     }
-    return axios.post(URL_BACKEND, data)
+    return axios.post(URL_BACKEND, data);
 }
 
 const updateUserAPI = () => {
 
 }
 
+const fetchAllUserAPI = () => {
+    const URL_BACKEND = "/api/v1/user";
+
+    return axios.get(URL_BACKEND);
+}
 
 export {
-    createUserAPI, updateUserAPI
+    createUserAPI, updateUserAPI, fetchAllUserAPI
 }
