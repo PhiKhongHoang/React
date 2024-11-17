225781e6e4fdd87c11e1e8a6d31e3d4ce1673be9
Menunoai <phik07tx02102002@gmail.com>
2024-11-17 20:58:04 +0700

    video 83

--- src/components/user/view.user.detail.jsx
+++ src/components/user/view.user.detail.jsx
@@ -4,6 +4,25 @@ import { useEffect, useState } from "react";
 const ViewUserDetail = (props) => {
     const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen, } = props;
 
+    const [selectedFile, setSelectedFile] = useState(null)
+    const [preview, setPreview] = useState(null)
+
+
+    const handleOnchangeFile = (event) => {
+        if (!event.target.files || event.target.files.length === 0) {
+            setSelectedFile(null)
+            setPreview(null)
+            return;
+        }
+
+        // I've kept this example simple by using the first image instead of multiple
+        const file = event.target.files[0]
+        if (file) {
+            setSelectedFile(file)
+            setPreview(URL.createObjectURL(file))
+        }
+    }
+    console.log(">>> check file: ", preview)
 
     return (
         <>
@@ -28,10 +47,20 @@ const ViewUserDetail = (props) => {
                         <p>Phone: {dataDetail.phone}</p>
                         <br />
                         <p>Avatar: </p>
-                        <div>
+                        <div
+                            style={{
+                                marginTop: "10px",
+                                height: "100px",
+                                width: "150px",
+                                border: "1px solid #ccc"
+                            }}
+                        >
                             <img
-                                height={100} width={150}
-                                src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} />
+                                style={{
+                                    height: "100%", width: "100%", objectFit: "contain"
+                                }}
+                                src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`}
+                            />
                         </div>
                         <div>
                             <label
@@ -48,9 +77,30 @@ const ViewUserDetail = (props) => {
                             >
                                 Upload avatar
                             </label>
-                            <input type="file" hidden id="btnUpload" />
+                            <input
+                                type="file" hidden id="btnUpload"
+                                // onChange={handleOnchangeFile}
+                                onChange={(event) => handleOnchangeFile(event)}
+                            />
                         </div>
-                        {/* <Button type="primary">Upload avatar</Button> */}
+
+                        {preview &&
+                            <div
+                                style={{
+                                    marginTop: "10px",
+                                    height: "100px",
+                                    width: "150px",
+                                    border: "1px solid #ccc"
+                                }}
+                            >
+                                <img
+                                    style={{
+                                        height: "100%", width: "100%", objectFit: "contain"
+                                    }}
+                                    src={preview}
+                                />
+                            </div>
+                        }
                     </>
                     :
                     <>
